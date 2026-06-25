5e5f238f964acc9e5c35e38ebe181defe6690153
a88981 <maru149owo@gmail.com>
2026-06-25 15:24:52 +0800

    Refactor milestone and rewardItem handling in rewards.js

--- api/rewards.js
+++ api/rewards.js
@@ -48,7 +48,7 @@ module.exports = async function handler(req, res) {
             '觸發日期': { title:     [{ text:{ content: triggerDate||'' } }] },
             '會員':     { relation:  [{ id: memberId }] },
             '獎勵類型': { select:    { name: type } },
-            '里程碑':   { select:    { name: milestone } },
+            ...(milestone ? { '里程碑': { select: { name: milestone } } } : {}),
             ...(rewardItem ? { '兌換內容': { select: { name: rewardItem } } } : {}),
             '指定插圖': { rich_text: [{ text:{ content: illustration||'' } }] },
             '已兌換':   { checkbox:  false },
@@ -66,7 +66,7 @@ module.exports = async function handler(req, res) {
       const props = {};
       if (redeemed     !== undefined) props['已兌換']   = { checkbox: redeemed };
       if (redeemedAt   !== undefined) props['兌換日期'] = { date: redeemedAt ? { start: redeemedAt } : null };
-      if (rewardItem   !== undefined) props['兌換內容'] = rewardItem ? { select: { name: rewardItem } } : { select: null };
+      if (rewardItem   !== undefined) props['兌換內容'] = { select: { name: rewardItem } };
       if (illustration !== undefined) props['指定插圖'] = { rich_text: [{ text:{ content: illustration } }] };
       if (note         !== undefined) props['備註']     = { rich_text: [{ text:{ content: note } }] };
       const r = await fetch(`https://api.notion.com/v1/pages/${id}`, {
